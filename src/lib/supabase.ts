2302376555141dd39f0971986518c643dcddf278
webflixinfo <kontakt@webflix.info>
2026-02-23 17:07:19 +0100

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://0ec90b57d6e95fcbda19832f.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNzM2MDAsImV4cCI6MjA1NTY0OTYwMH0.kQZ0n3h9y3qQ7yP_B0pGx8xK4f5dR6jT9vW2nN5oM8g'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
